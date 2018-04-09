d0964b8b6ec62e12ce9f07e97646ee4b1852f548
Antonio <antoniowow20111@hotmail.com>
2018-04-09 09:19:18 -0300

    Adicionando arquivo com funcoes geometricas

---
+++ geo.py
@@ -0,0 +1,20 @@
+def area_circulo(r):
+	return 3.14*(r**2)
+	
+def area_quadrado(l):
+    return l*l
+    
+def area_triangulo(b,a):
+	return (b*a)/2
+	
+def area_retangulo(b,a):
+	return b*a
+	
+def area_trapezio(b1,b2,a):
+	return ((b1+b2)*a/2)
+	
+def area_losango(d1,d2):
+	return (d1*d2)/2			    
+    
+def area_pentagono(a,b):
+	return (a*b)/2	    
